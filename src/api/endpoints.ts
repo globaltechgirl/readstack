9401210fff56afe064d075df5f3801d4524fe560
onyinyeofili <onyinyeofili209@gmail.com>
2025-11-24 12:24:45 +0100

    Updated

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -5,7 +5,7 @@ export const ENDPOINTS = {
     ME: "auth/me",
   },
   SHELVES: {
-    GET_ALL: "books",
+    GET_ALL: "books/OL257943W",
     GET_BY_ID: (id: number | string) => `books/${id}`,
     SEARCH_DB: "books/search",
     SEARCH_EXTERNAL: "books/search/external",
